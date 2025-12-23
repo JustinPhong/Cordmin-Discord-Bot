9d9b9e1ad932be185b783f749d300aa7a225370a
JustinPhong <93855379+JustinPhong@users.noreply.github.com>
2025-12-23 15:53:52 +0800

    Add new feature - Log to file

--- utils/core_helper.py
+++ utils/core_helper.py
@@ -1,42 +1,62 @@
 import asyncio
+import config
 import sys
+import os
+import re
+import json
 from datetime import datetime
 from colorama import init, Fore
 
-init()
+init(autoreset=True)
+ANSI_ESCAPE = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
 
-async def restart(error: str) -> bool:
-    answer = await async_input(f"[{datetime.now().strftime("%H:%M:%S")}] {Fore.YELLOW}{error}{Fore.RESET}, press Y to restart:\n", timeout=5)
+def servertime() -> str:
+    return f"[{datetime.now().strftime('%H:%M:%S')}]"
+
+def log(text: str):
+    time = servertime()
+    message = f"{time} {text}"
+
+    print(message)
+
+    if getattr(config, "LOG_FILE", None):
+        clean = strip_ansi(message)
+        save_file(clean,filename=datetime.now().strftime("%d-%m-%Y.log"),folder="Logs")
+
+
+def success(text: str):
+    log(f"{Fore.GREEN}{text}")
+
+def warning(text: str, e: Exception | None = None):
+    if e:
+        log(f"{Fore.YELLOW}{text}: {e}")
+    else:
+        log(f"{Fore.YELLOW}{text}")
+
+def error(text: str, e: Exception | None = None):
+    if e:
+        log(f"{Fore.RED}{text}: {e}")
+    else:
+        log(f"{Fore.RED}{text}")
+
+async def restart(error_msg: str) -> bool:
+    answer = await async_input(f"{servertime()} {Fore.YELLOW}{error_msg}{Fore.RESET}, press Y to restart: ",timeout=5)
     if answer and answer.strip().lower() == "y":
         log("Restarting...")
         return True
-    else:
-        return False
+    return False
 
 async def async_input(prompt: str, timeout: int = 10):
     loop = asyncio.get_running_loop()
     try:
-        return await asyncio.wait_for(loop.run_in_executor(None, lambda: input(prompt)), timeout)
+        return await asyncio.wait_for(loop.run_in_executor(None, input, prompt),timeout)
     except asyncio.TimeoutError:
         return None
 
-def error(text: str, e: Exception | None = None):
-    if e:
-        log(f"{Fore.RED}{text}: {e}{Fore.RESET}")
-    else:
-        log(f"{Fore.RED}{text}{Fore.RESET}")
+def save_file(data: str, filename: str, folder: str):
+    os.makedirs(folder, exist_ok=True)
+    filepath = os.path.join(folder, filename)
+    with open(filepath, "a", encoding="utf-8") as f: f.write(data + "\n")
 
-def warning(text: str, e: Exception | None = None):
-    if e:
-        log(f"{Fore.YELLOW}{text}: {e}{Fore.RESET}")
-    else:
-        log(f"{Fore.YELLOW}{text}{Fore.RESET}")
-
-def success(text: str):
-    log(f"{Fore.GREEN}{text}{Fore.RESET}")
-
-def log(text:str):
-    print(f"[{datetime.now().strftime("%H:%M:%S")}] {text}")
-
-def servertime() -> str:
-    return f"[{datetime.now().strftime("%H:%M:%S")}]"
+def strip_ansi(text: str) -> str:
+    return ANSI_ESCAPE.sub("", text)
