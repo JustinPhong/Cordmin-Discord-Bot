fb34401af60935dc884c2a3684ba3518f32373c4
JustinPhong <93855379+JustinPhong@users.noreply.github.com>
2025-12-24 10:56:43 +0800

    Add new feature - Generate user config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import discord
+import toml
 import os
 from dotenv import load_dotenv
 
@@ -11,17 +12,32 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 NOTIFICATION_MSG_ID = os.getenv("NOTIFICATION_MSG_ID")
 NOTIFICATION_CHANNEL = os.getenv("NOTIFICATION_CHANNEL")
 VOICE_TEMP_CATEGORY = os.getenv("VOICE_TEMP_CATEGORY")
-VOICE_TEMP_CHANNEL= os.getenv("VOICE_TEMP_CHANNEL")
+VOICE_TEMP_CHANNEL = os.getenv("VOICE_TEMP_CHANNEL")
 ANNOUNCEMENT_CHANNEL = os.getenv("ANNOUNCEMENT_CHANNEL")
-LOG_CHANNEL= os.getenv("LOG_CHANNEL")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
 
 intents = discord.Intents.all()
 intents.message_content = True
 intents.guilds = True
 intents.members = True
 
-LOG_MCSTATUS = False
-COOLDOWN_SECONDS = 300
-CHECK_INTERVAL = 60
+USER_CONFIG_PATH = "Config"
 
-LOG_FILE = True
+if os.path.isfile(USER_CONFIG_PATH):
+    with open(USER_CONFIG_PATH, "r") as f:
+        user_config = toml.load(f)
+else:
+    default_config = """LOG_MCSTATUS = false
+#Cooldown period if server is offline, minimum 1 second
+COOLDOWN_SECONDS = 300
+#Check server status interval in seconds, minimum 1 second
+CHECK_INTERVAL = 60
+LOG_FILE = true"""
+    with open(USER_CONFIG_PATH, "w") as f:
+        f.write(default_config)
+        user_config = toml.loads(default_config)
+
+LOG_MCSTATUS = user_config.get("LOG_MCSTATUS", False)
+COOLDOWN_SECONDS = max(user_config.get("COOLDOWN_SECONDS", 1), 1)
+CHECK_INTERVAL = max(user_config.get("CHECK_INTERVAL", 1), 1)
+LOG_FILE = user_config.get("LOG_FILE", True)
